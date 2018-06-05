683cdb2707a065d5f0a4272e7bab8a91a7a2cdcb
patrickz98 <patrickz98@users.noreply.github.com>
2018-06-05 16:49:39 +0200

    Check in. 2018-06-05 16:49

--- src/github.com/patrickz98/GoGo/Mongodb.go
+++ src/github.com/patrickz98/GoGo/Mongodb.go
@@ -6,15 +6,19 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"github.com/mongodb/mongo-go-driver/bson"
 )
+
 
 const MONGO_SERVER = "mongodb://localhost:27017"
 const MONGO_Database = "examples"
 const MONGO_Collection = "test"
 
 type Person struct {
-	Name string
-	Phone string
+	Name      string
+	Phone     string
+	createdAt time.Time
+	created   time.Time
 }
 
 func init() {
@@ -29,6 +33,32 @@ func RandStringRunes(n int) string {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(b)
+}
+
+func createIndex(coll *mongo.Collection) {
+	//
+	// Create Index
+	//
+
+	// expireAt := bson.NewDocument(bson.EC.Int32("expireAt", 1))
+	// expireAfterSeconds := bson.NewDocument(bson.EC.Int64("expireAfterSeconds", 0))
+
+	expireAt := bson.NewDocument(bson.EC.Int32("createdAt", 1))
+	expireAfterSeconds := bson.NewDocument(bson.EC.Int32("expireAfterSeconds", 10))
+	// expireAfterSeconds := bson.NewDocument(bson.EC.Int64("expireAfterSeconds", 6 * 7 * 24 * 60 * 60))
+
+	indexModel := mongo.IndexModel{
+		Keys:    expireAt,
+		Options: expireAfterSeconds,
+	}
+
+	indexResult, err := coll.Indexes().CreateOne(context.Background(), indexModel)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("indexResult=" + indexResult)
 }
 
 func mongoSample() {
@@ -53,7 +83,15 @@ func mongoSample() {
 	}
 
 	db := client.Database(MONGO_Database)
+
+	db.RunCommand(
+		context.Background(),
+		bson.NewDocument(bson.EC.Int32("dropDatabase", 1)),
+	)
+
 	coll := db.Collection(MONGO_Collection)
+
+	createIndex(coll)
 
 	// json := bson.NewDocument(
 	// 	bson.EC.String("item", "canvas"),
@@ -70,11 +108,39 @@ func mongoSample() {
 
 	// result, err := coll.InsertOne(context.Background(), json)
 
-	pups := Person{RandStringRunes(10), "iPhone"}
+	// str := "2018-06-05T16:01:26.371Z"
+	// removeTime, err := time.Parse(time.RFC3339, str)
+	//
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// pups := Person{RandStringRunes(10), "iPhone", removeTime}
+
+	// bson.EC.DateTime()
+	// pups := Person{RandStringRunes(10), "iPhone", time.Now(), time.Now()}
+	// pups := Person{RandStringRunes(10), "iPhone", removeTime, removeTime}
+
+	pups := bson.NewDocument(
+		bson.EC.String("Name", RandStringRunes(10)),
+		bson.EC.String("Item", "canvas"),
+		bson.EC.Time("created", time.Now().UTC()),
+		bson.EC.Time("createdAt", time.Now().UTC()),
+		// bson.EC.DateTime("created", time.Now().Unix()),
+		// bson.EC.DateTime("createdAt", time.Now().Unix()),
+	)
+
+	fmt.Println("pups")
+	fmt.Println(pups)
+
 	result, err := coll.InsertOne(context.Background(), pups)
 
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("result")
-	fmt.Println(result)
+	fmt.Println(result.InsertedID)
 
 	client.Disconnect(context.Background())
 }
